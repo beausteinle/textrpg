fb7981f9ae2129f1beb9bc0599375c66af01fef1
jgaskill01 <115852073+jgaskill01@users.noreply.github.com>
2024-02-26 00:06:26 -0600

    BJF-11: Get Scene by Region and Location ID (#7)
    * refactor class names; add regionDAOImpl/regionDAO
    
    * added getByLocation and extractSceneObj methods; added new query to DAOs for location_key

--- src/main/java/textrpg/dao/implementations/SceneDAOImpl.java
+++ src/main/java/textrpg/dao/implementations/SceneDAOImpl.java
@@ -1,17 +1,42 @@
 package textrpg.dao.implementations;
 
+import textrpg.dao.SqliteConnection;
+import textrpg.dao.interfaces.SceneDAO;
+import textrpg.models.Scene;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import textrpg.dao.SqliteConnection;
-import textrpg.dao.interfaces.SceneDAO;
-import textrpg.models.Scene;
-
 public class SceneDAOImpl implements SceneDAO {
 
     public static final String GET_SCENE_BY_ID_QUERY = "select * from scene where scene_id = ?";
+    public static final String GET_SCENE_BY_LOCATION_KEY_QUERY = "select * from scene where location_key = ? and region_id = ?";
+
+    @Override
+    public Scene getByLocation(int region_id, String location_key) {
+        try (Connection conn = SqliteConnection.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement(GET_SCENE_BY_LOCATION_KEY_QUERY);
+            stmt.setString(1, location_key);
+            stmt.setInt(2, region_id);
+
+            ResultSet result = stmt.executeQuery();
+            return extractSceneObj(result);
+        } catch (SQLException err) {
+            System.err.println(err.getMessage());
+        }
+        return null;
+    }
+
+    private Scene extractSceneObj(ResultSet queryResult) throws SQLException {
+        String title = queryResult.getString("title");
+        String description = queryResult.getString("description");
+        String locationKey = queryResult.getString("location_key");
+
+
+        return new Scene(title, description, locationKey);
+    }
 
     public Scene getById(int scene_id) {
         try (Connection conn = SqliteConnection.getConnection()) {
@@ -20,10 +45,7 @@ public class SceneDAOImpl implements SceneDAO {
 
             ResultSet result = stmt.executeQuery();
 
-            String title = result.getString("title");
-            String description = result.getString("description");
-
-            return new Scene(title, description);
+            return extractSceneObj(result);
         } catch (SQLException err) {
             System.err.println(err.getMessage());
         }
