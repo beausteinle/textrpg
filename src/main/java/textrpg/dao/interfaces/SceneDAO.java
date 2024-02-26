29f56a0384b46f5a2f16ad7ff995edd7b54980d0
jgaskill01 <115852073+jgaskill01@users.noreply.github.com>
2024-02-25 23:55:14 -0600

    added getByLocation and extractSceneObj methods; added new query to DAOs for location_key

--- src/main/java/textrpg/dao/interfaces/SceneDAO.java
+++ src/main/java/textrpg/dao/interfaces/SceneDAO.java
@@ -4,4 +4,5 @@ import textrpg.models.Scene;
 
 public interface SceneDAO {
     public Scene getById(int scene_id);
+    public Scene getByLocation(int region_id, String location_key);
 }
--- src/main/java/textrpg/models/Scene.java
+++ src/main/java/textrpg/models/Scene.java
@@ -3,16 +3,19 @@ package textrpg.models;
 public class Scene {
     String title;
     String description;
-
+    String locationKey;
     Action[] actions;
 
-    public Scene(String title, String description) {
+    public Scene(String title, String description, String locationKey) {
         this.title = title;
         this.description = description;
+        this.locationKey = locationKey;
     }
 
+
+
     public Scene() {
-        this("Empty Scene", "");
+        this("Empty Scene", "", "");
     }
 
     public String getTitle() {
