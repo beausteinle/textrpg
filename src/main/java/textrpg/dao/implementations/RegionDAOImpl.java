6b9717535d810651c2d34b776bd3291bfbceb66e
jgaskill01 <115852073+jgaskill01@users.noreply.github.com>
2024-02-25 21:48:56 -0600

    refactor class names; add regionDAOImpl/regionDAO

--- src/main/java/textrpg/dao/implementations/RegionDAOImpl.java
+++ src/main/java/textrpg/dao/implementations/RegionDAOImpl.java
@@ -13,11 +13,11 @@ import java.sql.SQLException;
 
 public class RegionDAOImpl implements RegionDAO {
 
-    public static final String GET_SCENE_BY_ID_QUERY = "select * from scene where scene_id = ?";
+    public static final String GET_REGION_BY_ID_QUERY = "select * from region where region_id = ?";
 
     public Region getById(int region_id) {
         try (Connection conn = SqliteConnection.getConnection()) {
-            PreparedStatement stmt = conn.prepareStatement(GET_SCENE_BY_ID_QUERY);
+            PreparedStatement stmt = conn.prepareStatement(GET_REGION_BY_ID_QUERY);
             stmt.setInt(1, region_id);
 
             ResultSet result = stmt.executeQuery();
