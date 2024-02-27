2ae40b3e7efde60e3e421a3433ad4e64658ea772
Beau Steinle <beausteinle@live.com>
2024-02-26 23:53:09 -0700

    Add world state dao

---
+++ src/main/java/textrpg/dao/implementations/WorldStateDAOImpl.java
@@ -0,0 +1,47 @@
+package textrpg.dao.implementations;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import textrpg.dao.SqliteConnection;
+import textrpg.dao.interfaces.WorldStateDAO;
+import textrpg.models.CharacterState;
+import textrpg.models.WorldState;
+import textrpg.utils.WorldPosition;
+
+public class WorldStateDAOImpl implements WorldStateDAO {
+  public static final String GET_WORLD_STATE_BY_ID =
+      """
+        select
+          ws.world_state_id,
+          cs.current_region_id as region_id,
+          cs.x_position,
+          cs.y_position
+        from world_state ws
+          join character_state cs on ws.character_state_id = cs.character_state_id
+        where ws.world_state_id = ?
+      """;
+
+  @Override
+  public WorldState getWorldStateById(int id) {
+
+    try (Connection conn = SqliteConnection.getConnection()) {
+      PreparedStatement stmt = conn.prepareStatement(GET_WORLD_STATE_BY_ID);
+      stmt.setInt(1, id);
+
+      ResultSet result = stmt.executeQuery();
+
+      int worldStateId = result.getInt("world_state_id");
+      int regionId = result.getInt("region_id");
+      int xPosition = result.getInt("x_position");
+      int yPosition = result.getInt("y_position");
+
+      return new WorldState(
+          worldStateId, new CharacterState(new WorldPosition(regionId, xPosition, yPosition)));
+    } catch (SQLException err) {
+      System.err.println(err.getMessage());
+    }
+    return null;
+  }
+}
--- src/main/java/textrpg/models/WorldState.java
+++ src/main/java/textrpg/models/WorldState.java
@@ -1,10 +1,12 @@
 package textrpg.models;
 
 public class WorldState {
+  int worldStateId;
   CharacterState characterState;
 
-  public WorldState(CharacterState characterState) {
+  public WorldState(int id, CharacterState characterState) {
     this.characterState = characterState;
+    worldStateId = id;
   }
 
   public CharacterState getCharacterState() {
