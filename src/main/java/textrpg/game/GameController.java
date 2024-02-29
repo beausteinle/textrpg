b8feba78d20c5136c349b009f59cab03a5cd737a
jgaskill01 <115852073+jgaskill01@users.noreply.github.com>
2024-02-28 23:45:09 -0600

    Initialization of app, simple game loop.

--- src/main/java/textrpg/game/GameController.java
+++ src/main/java/textrpg/game/GameController.java
@@ -3,7 +3,7 @@ package textrpg.game;
 import java.util.Scanner;
 import textrpg.dao.implementations.SceneDAOImpl;
 import textrpg.dao.interfaces.SceneDAO;
-import textrpg.models.Action;
+import textrpg.models.CharacterState;
 import textrpg.models.Scene;
 import textrpg.models.WorldState;
 import textrpg.user_interface.CmdLineInterface;
@@ -13,6 +13,7 @@ import textrpg.utils.WorldPosition;
 public class GameController {
   Scanner in;
   WorldState worldState;
+  SceneDAO sceneDAO = new SceneDAOImpl();
 
   Scene currentScene;
 
@@ -22,13 +23,29 @@ public class GameController {
     in = new Scanner(System.in);
   }
 
+  public GameController(WorldState worldState) {
+    in = new Scanner(System.in);
+    this.worldState = worldState;
+
+    CharacterState characterState = worldState.getCharacterState();
+    WorldPosition worldPosition = characterState.getWorldPosition();
+
+    currentScene = sceneDAO.getByLocation(worldPosition);
+  }
+
   public void playScene(Scene scene) {
     cmdInterface.displayScene(scene);
-    cmdInterface.displayActions(scene.getActions());
+    //    cmdInterface.displayActions(scene.getActions());
 
-    Action selectedAction = cmdInterface.getUserSelection(scene);
+    //    Action selectedAction = cmdInterface.getUserSelection(scene);
+    //
+    //    System.out.println(selectedAction.getResult().getResultDescription());
 
-    System.out.println(selectedAction.getResult().getResultDescription());
+    currentScene = moveCharacter();
+  }
+
+  public void playCurrentScene() {
+    playScene(currentScene);
   }
 
   /**
---
+++ src/main/java/textrpg/user_interface/NavHandler.java
@@ -0,0 +1,3 @@
+package textrpg.user_interface;
+
+public class NavHandler {}
